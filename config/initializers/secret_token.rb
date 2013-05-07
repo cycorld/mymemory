86063ab85223483cb9839919a097bcf46fb250ce
Charles <charleschoi87@gmail.com>
2013-05-08 00:37:16 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymemory::Application.config.secret_token = '445bc730fa8dcd4bdbf4662be9b5b8ffca621b08762710fc4dacf823ea0b638988f19b77ae7df2ef78f969889b041de5b6a0335c41c0ad0ac737cb81b7c8befb'
