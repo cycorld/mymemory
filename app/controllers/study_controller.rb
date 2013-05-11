1340a52e8a2c964360244ec8e5d995c546020668
Charles <charleschoi87@gmail.com>
2013-05-11 23:51:18 +0900

    my course del activate master

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -3,16 +3,30 @@ class StudyController < ApplicationController
     @courses = Course.order(:user_number) #사용자순 출력
     @user = User.find(1)
     #@subscribe = @user.courses
-    course_ids = Subscribe.where(:user_id => 1).map{|x| x.course_id}
+    course_ids = Subscribe.where(:user_id => 1, :activated => true).map{|x| x.course_id}
     @subscribe = Course.find_all_by_id(course_ids)
 
   end
+
   def subscribe
-    subscribe = Subscribe.new
-    subscribe.user_id = 1 #session_id
-    subscribe.course_id = params[:id]
-    subscribe.activated = true
-    subscribe.save
+    if(s = Subscribe.where(:user_id => 1, :course_id => params[:id]).empty?)
+      subscribe = Subscribe.new
+      subscribe.user_id = 1 #session_id
+      subscribe.course_id = params[:id]
+      subscribe.activated = true
+      subscribe.save
+    else
+      sub = Subscribe.where(:user_id => 1, :course_id => params[:id]).first
+      sub.activated = true
+      sub.save
+    end
+    redirect_to '/study/index'
+  end
+
+  def unsubscribe
+    del = Subscribe.where(:user_id => 1, :course_id => params[:id]).first
+    del.activated = false
+    del.save
     redirect_to '/study/index'
   end
 end
